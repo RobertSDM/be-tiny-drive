7c92bcf346cd14b51d282917cbe5802060f60c1d
Roberto <roberto.menesantos@gmail.com>
2024-06-21 09:24:49 -0300

    removing: cors

--- main.py
+++ main.py
@@ -10,21 +10,20 @@ dotenv.load_dotenv()
 app = FastAPI(title="Tiny Drive", )
 
 # CORS config
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins="*",
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins="*",
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(file_route)
 app.include_router(folder_router)
     
 port = int(os.environ.get("PORT") if os.environ.get("PORT") else 4500)
 debug = "info" if os.environ.get("MODE") != "production" else "debug"
-host = os.environ.get("HOST") if os.environ.get("HOST") else "127.0.0.1"
+host = os.environ.get("HOST") if os.environ.get("HOST") else "0.0.0.0"
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=host, port=port, reload=True, log_level=debug)
