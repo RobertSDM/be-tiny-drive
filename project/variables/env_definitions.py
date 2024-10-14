b02e1d9ab2602c670857bdd63d24edffbd0b4acd
Roberto <roberto.menesantos@gmail.com>
2024-10-14 07:42:27 -0300

    fix: fixing the clients_url env variable

--- project/variables/env_definitions.py
+++ project/variables/env_definitions.py
@@ -1,6 +1,9 @@
 import dotenv, os
 
 dotenv.load_dotenv()
+
+print(os.environ.get("CLIENTS_URL"))
+
 # Global enviroment variables
 Secret_key = os.environ.get("SECRET_KEY")
 Database_url = os.environ.get("DATABASE_URL")
@@ -8,4 +11,4 @@ Origins = os.environ.get("ORIGINS").split(";")
 Port = os.environ.get("PORT") if os.environ.get("PORT") else "4500"
 Debug = "info" if os.environ.get("MODE") != "production" else "debug"
 Host = os.environ.get("HOST") if os.environ.get("HOST") else "0.0.0.0"
-Clients_URL = os.environ.get("CLIENTS__URL")
+Clients_URL = os.environ.get("CLIENTS_URL")
