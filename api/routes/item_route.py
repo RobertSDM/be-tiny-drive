b43f2b57a3fc2fcd3f30bfc65a28385dd4e10dd6
Roberto <roberto.menesantos@gmail.com>
2025-05-11 22:30:41 -0300

    feat: adding a route to get the items from a folder

--- api/routes/item_route.py
+++ api/routes/item_route.py
@@ -8,6 +8,7 @@ from database.db_engine import get_session
 from database.models.enums.content_type import ItemType
 from service.item_serv import (
     delete_item_serv,
+    get_all_items_in_folder,
     get_all_root_items_serv,
     item_create_serv,
 )
@@ -47,9 +48,16 @@ def save_file_route(
     return JSONResponse(SingleItemResponse(data=item).model_dump())
 
 
-@item_router.get("/root/all/{ownerid}")
+@item_router.get("/all/{ownerid}")
 def all_items(ownerid: int, db=Depends(get_session)):
     items = get_all_root_items_serv(db, ownerid)
+
+    return JSONResponse(ListItemResponse(data=items, count=len(items)).model_dump())
+
+
+@item_router.get("/all/{ownerid}/{parentid}")
+def all_item_in_folder(ownerid: int, parentid: int, db=Depends(get_session)):
+    items = get_all_items_in_folder(db, ownerid, parentid)
 
     return JSONResponse(ListItemResponse(data=items, count=len(items)).model_dump())
 
