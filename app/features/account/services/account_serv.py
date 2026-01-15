7bd904f1be216e63ace81adb501163e894340e07
RobertSDM <roberto.menesantos@gmail.com>
2026-01-15 20:23:43 -0300

    refactor: completely removing the query utils

--- app/features/account/services/account_serv.py
+++ app/features/account/services/account_serv.py
@@ -2,11 +2,10 @@ from sqlalchemy.orm import Session
 
 from app.core.exceptions import AccountNotExists
 from app.database.repositories.account_repo import account_by_id
-from app.utils.query import exec_first
 
 
 def account_get_serv(db: Session, id: str):
-    account = exec_first(account_by_id(db, id))
+    account = account_by_id(db, id).first()
     if not account:
         raise AccountNotExists()
 
