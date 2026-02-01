82c8f35f830417fc7c15816eaa759b6f489bc9b6
RobertSDM <roberto.menesantos@gmail.com>
2026-02-01 10:52:55 -0300

    refactor: creating a constant to list all supported preview s

--- app/features/file/services/FileDeleteService.py
+++ app/features/file/services/FileDeleteService.py
@@ -2,7 +2,7 @@ from typing import List
 from sqlalchemy.orm import Session
 from storage3.exceptions import StorageApiError
 
-from app.core.constants import SUPA_BUCKETID
+from app.core.constants import SUPA_BUCKETID, SUPPORTED_PREVIEW_TYPES
 from app.core.exceptions import FileNotFound
 from app.lib.supabase.storage import supabase_storage_client
 from app.database.models import FileModel
@@ -30,32 +30,32 @@ class FileDeleteService:
             else:
                 self._move_to_trash(ownerid, file)
 
-    def _move_from_trash(self, ownerid: str, file: FileModel):
+    def _move_from_trash(self, ownerid: str, fileid: str, content_type: str):
         supabase_storage_client.move(
             SUPA_BUCKETID,
-            make_file_bucket_path(ownerid, file.id, "trash+file"),
-            make_file_bucket_path(ownerid, file.id, "file"),
+            make_file_bucket_path(ownerid, fileid, "trash+file"),
+            make_file_bucket_path(ownerid, fileid, "file"),
         )
 
-        if file.content_type.startswith("image"):
+        if content_type in SUPPORTED_PREVIEW_TYPES:
             supabase_storage_client.move(
                 SUPA_BUCKETID,
-                make_file_bucket_path(ownerid, file.id, "trash+preview"),
-                make_file_bucket_path(ownerid, file.id, "preview"),
+                make_file_bucket_path(ownerid, fileid, "trash+preview"),
+                make_file_bucket_path(ownerid, fileid, "preview"),
             )
 
-    def _move_to_trash(self, ownerid: str, file: FileModel):
+    def _move_to_trash(self, ownerid: str, fileid: str, content_type: str):
         supabase_storage_client.move(
             SUPA_BUCKETID,
-            make_file_bucket_path(ownerid, file.id, "file"),
-            make_file_bucket_path(ownerid, file.id, "trash+file"),
+            make_file_bucket_path(ownerid, fileid, "file"),
+            make_file_bucket_path(ownerid, fileid, "trash+file"),
         )
 
-        if file.content_type.startswith("image"):
+        if content_type in SUPPORTED_PREVIEW_TYPES:
             supabase_storage_client.move(
                 SUPA_BUCKETID,
-                make_file_bucket_path(ownerid, file.id, "preview"),
-                make_file_bucket_path(ownerid, file.id, "trash+preview"),
+                make_file_bucket_path(ownerid, fileid, "preview"),
+                make_file_bucket_path(ownerid, fileid, "trash+preview"),
             )
 
     def delete_files(
@@ -71,12 +71,12 @@ class FileDeleteService:
                 if file.is_dir:
                     self._cascade_storage(db, ownerid, file.id)
                 else:
-                    self._move_to_trash(ownerid, file)
+                    self._move_to_trash(ownerid, file.id, file.content_type)
 
                 file_delete(db, file)
         except StorageApiError as e:
             for file in files:
-                self._move_from_trash(ownerid, file)
+                self._move_from_trash(ownerid, file.id, file.content_type)
 
             if e.code == "NoSuchUpload":
                 raise FileNotFound()
@@ -84,7 +84,7 @@ class FileDeleteService:
             raise e
         except Exception as e:
             for file in files:
-                self._move_from_trash(ownerid, file)
+                self._move_from_trash(ownerid, file.id, file.content_type)
 
             raise e
 
